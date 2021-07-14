e77811fb9bacecab0b024caf0a8268818570585e
JHerminio <kulitpo25@gmail.com>
2021-07-14 10:25:14 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import  profileImage from './img/ako.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>JHerminio</h1>
-      <img src={profileImage} alt="profile-image"/>
+
     </div>
   );
 }
